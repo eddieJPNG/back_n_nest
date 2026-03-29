8e0575a0702c0017adb3619bbf9e206aaf252158
Edson Rocha da Silva <eddiewav@eddiedev.eddiewav>
2026-03-29 09:58:33 -0300

    Fix some db erros and initiate the test on Jest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '@app/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('Users (e2e)', () => {
   let app: INestApplication<App>;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe())
     await app.init();
   });
 
